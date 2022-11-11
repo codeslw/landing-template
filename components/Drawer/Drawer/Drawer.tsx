f23f6da0c9c013609f47483813ca10424c715583
Saidkamol99 <sanvarkhujaev@gmail.com>
2022-11-11 18:02:24 +0500

    fix: fixed position of overlay

--- components/Drawer/Drawer/Drawer.tsx
+++ components/Drawer/Drawer/Drawer.tsx
@@ -13,7 +13,7 @@ export const Drawer: React.FC<DrawerProps> = ({ open, handleClose }) => {
 		<div onClick={(e) => {
 			e.stopPropagation()
 			handleClose()
-		}} className="absolute bg-black bg-opacity-50 w-screen h-screen top-0 left-0  sm:hidden"></div>
+		}} className="fixed bg-black pointer-events-none  bg-opacity-50 w-screen h-full min-h-full top-0 left-0 z-40  sm:hidden overflow-hidden"></div>
 		<div className="fixed w-80 h-screen left-0  top-0  bg-white px-0 py-4 flex flex-col items-center sapce-y-5 z-40 sm:hidden">
 			<div onClick={handleClose} className="flex w-full justify-end px-4">
 				<div className="w-8 h-8 rounded-lg hover:bg-gray-300 font-extrabold flex items-center justify-center text-xl">x</div>
